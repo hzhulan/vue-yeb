f0a6ff28d3b981b2bb1bfb4d9378880647fa3f90
hzhulan <zgjszhuhui@163.com>
2021-11-21 20:58:51 +0800

    服务调整

--- service/src/main/java/pri/pplan/controller/IndexController.java
+++ service/src/main/java/pri/pplan/controller/IndexController.java
@@ -2,15 +2,18 @@ package  pri.pplan.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pri.pplan.model.Response;
 
 /**
  * @authod: pp_lan on 2020/7/11.
  */
-@RequestMapping("/api/v1/index")
+@RestController
+@RequestMapping("/index")
 public class IndexController {
 
-    @RequestMapping(value = "index", method = {RequestMethod.GET, RequestMethod.POST})
-    public String index() {
-        return "index";
+    @RequestMapping(value = "/index", method = {RequestMethod.GET, RequestMethod.POST})
+    public Response index() {
+        return Response.ok("index");
     }
 }
