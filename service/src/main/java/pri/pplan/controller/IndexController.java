f06a7e75652dc4bd0d73a086f5ce205d6e3920ec
hzhulan <zgjszhuhui@163.com>
2021-11-21 20:41:48 +0800

    修改路徑

---
+++ service/src/main/java/pri/pplan/controller/IndexController.java
@@ -0,0 +1,16 @@
+package  pri.pplan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @authod: pp_lan on 2020/7/11.
+ */
+@RequestMapping("/api/v1/index")
+public class IndexController {
+
+    @RequestMapping(value = "index", method = {RequestMethod.GET, RequestMethod.POST})
+    public String index() {
+        return "index";
+    }
+}
